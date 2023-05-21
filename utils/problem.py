3e1077a8fd202c40837a84752ac275a46a7e7415
Lesi Chen <78390095+TrueNobility303@users.noreply.github.com>
2023-05-21 09:58:04 +0800

    Update problem.py

--- utils/problem.py
+++ utils/problem.py
@@ -85,133 +85,7 @@ def fast_mix(variable, comm, rank, size, kappa, K):
         variable_old = np.copy(variable)
     return variable
 
-## gradient_estimator: 'MR': momentum-based recursive approach; 'VR': variance reduction approach, 
-## consensus_methid: 'None' ; 'MC': multi-consensus; 'FM': FastMix
 
-# DMO (Decentralized Minimax Optimizer: for single loop decentralized NC-SC minimax)
-def DMO(data, label, x0, y0, args, comm, epochs, gradient_estimator='VR', consensus_method='FM'):
-    alpha = args.alpha
-    lambda2 = args.lambda2
-    kappa = args.kappa
-
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    # use different random seeds for each agent
-    np.random.seed(42 + rank)
-
-    n, d = data.shape
-    p = n // size
-    
-    # initialize 
-    x = np.copy(x0)
-    y = np.copy(y0)
-    x_old = np.copy(x)
-    gx_old = np.zeros_like(x)
-    vx = np.zeros_like(x)
-    x_bar = np.copy(x)
-    y_old = np.copy(y)
-    gy_old = np.zeros_like(y)
-    vy = np.zeros_like(y)
-
-    comm_comp_lst = []
-    grad_comp_lst = []
-    phi_lst = []
-    grad_cnt = 0
-    comm_cnt = 0
-    
-    min_phi = np.inf
-    for epoch in tqdm(range(epochs)):
-        # decentralized minimax optimizer 
-        if gradient_estimator == 'MR':
-            # we find that the initial large batch size may be ununcessary in practice
-            # if epoch == 0:
-            #     idx = np.random.randint(0, p, b0) * rank 
-            #     gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha)
-            #     gy = cal_gradient_y(data, label, x, y, idx, n)
-            #     grad_cnt = grad_cnt + b0
-            idx = np.random.randint(0, p, args.b) * rank
-            gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha) + (1 - args.beta) * \
-                (gx_old - cal_gradient_x(data, label, x_old, y_old, idx, lambda2, alpha))
-            gy = cal_gradient_y(data, label, x, y, idx, n) + (1 - args.beta) * \
-                (gy_old - cal_gradient_y(data, label, x_old, y_old, idx, n))
-            grad_cnt = grad_cnt + 4 * size * args.b
-        elif gradient_estimator == 'VR':
-            # set both the probability, mini-batch size and mega-batch size according to theoretical choice
-            b0 = args.b * args.b 
-            prob = args.b / (args.b + b0)
-            rad = np.random.random()
-            if rad < prob:
-                id0 = np.random.randint(0, p, b0) * rank
-                # id0 = range(n)
-                gx = cal_gradient_x(data, label, x, y, id0, lambda2, alpha)
-                gy = cal_gradient_y(data, label, x, y, id0, n)
-                grad_cnt = grad_cnt + 2 * size * b0
-            else:
-                idx = np.random.randint(0, p, args.b) * rank
-                gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha) + \
-                    (gx_old - cal_gradient_x(data, label, x_old, y_old, idx, lambda2, alpha))
-                gy = cal_gradient_y(data, label, x, y, idx, n) + \
-                    (gy_old - cal_gradient_y(data, label, x_old, y_old, idx, n))
-                grad_cnt = grad_cnt + 4 * size * args.b
-        else:
-            idx = np.random.randint(0, p, args.b) * rank 
-            gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha)
-            gy = cal_gradient_y(data, label, x, y, idx, n)
-            grad_cnt = grad_cnt + 2 * size * args.b
-        # gradient tracking
-        vx = vx + gx - gx_old
-        vy = vy + gy - gy_old
-        if consensus_method is None:
-            vx = consensus(vx, comm, rank, size, kappa)
-            vy = consensus(vy, comm, rank, size, kappa)
-        elif consensus_method == 'MC':
-            vx = multi_consensus(vx, comm, rank, size, kappa, args.num_mix)
-            vy = multi_consensus(vy, comm, rank, size, kappa, args.num_mix)
-        elif consensus_method == 'FM':
-            vx = fast_mix(vx, comm, rank, size, kappa, args.num_mix)
-            vy = fast_mix(vy, comm, rank, size, kappa, args.num_mix)
-            
-        gx_old = np.copy(gx)
-        gy_old = np.copy(gy)
-        # update x and y
-        x_old = np.copy(x)
-        x = x - args.lr_x * vx
-        y_old = np.copy(y)
-        y = y + args.lr_y * vy
-        if consensus_method is None:
-            x = consensus(x, comm, rank, size, kappa)
-            y = consensus(y, comm, rank, size, kappa)
-        elif consensus_method == 'MC':
-            x = multi_consensus(x, comm, rank, size, kappa, args.num_mix)
-            y = multi_consensus(y, comm, rank, size, kappa, args.num_mix)
-        elif consensus_method == 'FM':
-            x = fast_mix(x, comm, rank, size, kappa, args.num_mix)
-            y = fast_mix(y, comm, rank, size, kappa, args.num_mix)
-
-        y = projection(y, n)
-
-        if consensus_method is not None:
-            comm_cnt = comm_cnt + 4 * args.num_mix
-        else:
-            comm_cnt = comm_cnt + 4
-
-        if (epoch + 1) % args.print_freq == 0:
-            # compute x_bar on agent rank 0
-            comm.Reduce(x, x_bar, op=MPI.SUM)
-            if rank == 0:
-                x_bar = x_bar / size
-                phi,grad_phi = cal_phi(data, label, x_bar, n, lambda2, alpha)
-                with open(args.out_fname, '+a') as f:
-                    print('DMO with {cm}:{ep:d},{phi:.8f},{grad_phi:.8f}'.format(cm=gradient_estimator, ep=epoch + 1, phi=float(phi),grad_phi=float(grad_phi)), file=f)
-                # store the minimal loss
-                if phi < min_phi:
-                    min_phi = phi
-                phi_lst.append(phi / n)
-                grad_comp_lst.append(grad_cnt)
-                comm_comp_lst.append(comm_cnt)
-        comm.barrier()
-    return grad_comp_lst, comm_comp_lst, phi_lst
 
 # GT-GDA
 def GDA(data, label, x0, y0, args, comm, epochs):
@@ -370,3 +244,261 @@ def SRVR(data, label, x0, y0, args, comm, epochs):
         comm.barrier()
     return grad_comp_lst, comm_comp_lst, phi_lst
 
+# GT-DA
+def DA(data, label, x0, y0, args, comm, epochs):
+    alpha = args.alpha
+    lambda2 = args.lambda2
+    kappa = args.kappa
+
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # use different random seeds for each agent
+    np.random.seed(42 + rank)
+
+    n, d = data.shape
+    p = n // size
+    
+    # initialize 
+    x = np.copy(x0)
+    y = np.copy(y0)
+    x_old = np.copy(x)
+    gx_old = np.zeros_like(x)
+    vx = np.zeros_like(x)
+    x_bar = np.copy(x)
+    y_old = np.copy(y)
+    gy_old = np.zeros_like(y)
+    vy = np.zeros_like(y)
+
+    comm_comp_lst = []
+    grad_comp_lst = []
+    phi_lst = []
+    grad_cnt = 0
+    comm_cnt = 0
+    
+    min_phi = np.inf
+    for epoch in tqdm(range(epochs)):
+        idx = np.arange(p) * rank 
+        gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha)
+        gy = cal_gradient_y(data, label, x, y, idx, n)
+        grad_cnt = grad_cnt + 2 * size * p
+        # gradient tracking
+        vx = vx + gx - gx_old
+        vy = vy + gy - gy_old
+        vx = consensus(vx, comm, rank, size, kappa)
+        vy = consensus(vy, comm, rank, size, kappa)
+            
+        gx_old = np.copy(gx)
+        gy_old = np.copy(gy)
+        # update x and y
+        if epoch % args.multi_step == 0:
+            x_old = np.copy(x)
+            x = x - args.lr_x * vx
+        else:
+            y_old = np.copy(y)
+            y = y + args.lr_y * vy
+            # we ignore the gradient tracking step in y direction following option (2) in the paper.
+
+        x = consensus(x, comm, rank, size, kappa)
+        y = consensus(y, comm, rank, size, kappa)
+        y = projection(y, n)
+
+        comm_cnt = comm_cnt + 4
+
+        if (epoch + 1) % args.print_freq == 0:
+            # compute x_bar on agent rank 0
+            comm.Reduce(x, x_bar, op=MPI.SUM)
+            if rank == 0:
+                x_bar = x_bar / size
+                phi,grad_phi = cal_phi(data, label, x_bar, n, lambda2, alpha)
+                with open(args.out_fname, '+a') as f:
+                    print('GT-DA:{ep:d},{phi:.8f},{grad_phi:.8f}'.format(ep=epoch + 1, phi=float(phi),grad_phi=float(grad_phi)), file=f)
+                # store the minimal loss
+                if phi < min_phi:
+                    min_phi = phi
+                phi_lst.append(phi / n)
+                grad_comp_lst.append(grad_cnt)
+                comm_comp_lst.append(comm_cnt)
+        comm.barrier()
+    return grad_comp_lst, comm_comp_lst, phi_lst
+
+def DMHSGD(data, label, x0, y0, args, comm, epochs):
+    alpha = args.alpha
+    lambda2 = args.lambda2
+    kappa = args.kappa
+
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # use different random seeds for each agent
+    np.random.seed(42 + rank)
+
+    n, d = data.shape
+    p = n // size
+    
+    # initialize 
+    x = np.copy(x0)
+    y = np.copy(y0)
+    x_old = np.copy(x)
+    gx_old = np.zeros_like(x)
+    vx = np.zeros_like(x)
+    x_bar = np.copy(x)
+    y_old = np.copy(y)
+    gy_old = np.zeros_like(y)
+    vy = np.zeros_like(y)
+
+    comm_comp_lst = []
+    grad_comp_lst = []
+    phi_lst = []
+    grad_cnt = 0
+    comm_cnt = 0
+    
+    min_phi = np.inf
+    for epoch in tqdm(range(epochs)):
+        # decentralized minimax optimizer 
+       
+        # we find that the initial large batch size may be ununcessary in practice
+        # if epoch == 0:
+        #     idx = np.random.randint(0, p, b0) * rank 
+        #     gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha)
+        #     gy = cal_gradient_y(data, label, x, y, idx, n)
+        #     grad_cnt = grad_cnt + b0
+        idx = np.random.randint(0, p, args.b) * rank
+        gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha) + (1 - args.beta) * \
+            (gx_old - cal_gradient_x(data, label, x_old, y_old, idx, lambda2, alpha))
+        gy = cal_gradient_y(data, label, x, y, idx, n) + (1 - args.beta) * \
+            (gy_old - cal_gradient_y(data, label, x_old, y_old, idx, n))
+        grad_cnt = grad_cnt + 4 * size * args.b
+       
+        vx = consensus(vx, comm, rank, size, kappa)
+        vy = consensus(vy, comm, rank, size, kappa)
+    
+            
+        gx_old = np.copy(gx)
+        gy_old = np.copy(gy)
+        # update x and y
+        x_old = np.copy(x)
+        x = x - args.lr_x * vx
+        y_old = np.copy(y)
+        y = y + args.lr_y * vy
+
+        x = consensus(x, comm, rank, size, kappa)
+        y = consensus(y, comm, rank, size, kappa)
+        
+
+        y = projection(y, n)
+
+        comm_cnt = comm_cnt + 4
+
+        if (epoch + 1) % args.print_freq == 0:
+            # compute x_bar on agent rank 0
+            comm.Reduce(x, x_bar, op=MPI.SUM)
+            if rank == 0:
+                x_bar = x_bar / size
+                phi,grad_phi = cal_phi(data, label, x_bar, n, lambda2, alpha)
+                with open(args.out_fname, '+a') as f:
+                    print('DMHSGD: {ep:d},{phi:.8f},{grad_phi:.8f}'.format(ep=epoch + 1, phi=float(phi),grad_phi=float(grad_phi)), file=f)
+                # store the minimal loss
+                if phi < min_phi:
+                    min_phi = phi
+                phi_lst.append(phi / n)
+                grad_comp_lst.append(grad_cnt)
+                comm_comp_lst.append(comm_cnt)
+        comm.barrier()
+    return grad_comp_lst, comm_comp_lst, phi_lst
+
+def DREAM(data, label, x0, y0, args, comm, epochs):
+    alpha = args.alpha
+    lambda2 = args.lambda2
+    kappa = args.kappa
+
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # use different random seeds for each agent
+    np.random.seed(42 + rank)
+
+    n, d = data.shape
+    p = n // size
+    
+    # initialize 
+    x = np.copy(x0)
+    y = np.copy(y0)
+    x_old = np.copy(x)
+    gx_old = np.zeros_like(x)
+    vx = np.zeros_like(x)
+    x_bar = np.copy(x)
+    y_old = np.copy(y)
+    gy_old = np.zeros_like(y)
+    vy = np.zeros_like(y)
+
+    comm_comp_lst = []
+    grad_comp_lst = []
+    phi_lst = []
+    grad_cnt = 0
+    comm_cnt = 0
+    
+    min_phi = np.inf
+    for epoch in tqdm(range(epochs)):
+        # decentralized minimax optimizer 
+        # args b0,b, prob and q
+        
+        rad1 = np.random.random()
+        
+        if rad1 < args.prob:
+            id0 = np.random.randint(0, p, args.b0) * rank
+            # id0 = range(n)
+            gx = cal_gradient_x(data, label, x, y, id0, lambda2, alpha)
+            gy = cal_gradient_y(data, label, x, y, id0, n)
+            grad_cnt = grad_cnt + 2 * size * args.b0
+        else:
+            rad2 = np.random.random()
+            if rad2 < args.q:
+                idx = np.random.randint(0, p, args.b) * rank
+                gx = cal_gradient_x(data, label, x, y, idx, lambda2, alpha) + \
+                    1/args.q * (gx_old - cal_gradient_x(data, label, x_old, y_old, idx, lambda2, alpha))
+                gy = cal_gradient_y(data, label, x, y, idx, n) + \
+                    1/args.q * (gy_old - cal_gradient_y(data, label, x_old, y_old, idx, n)) 
+                grad_cnt = grad_cnt + 4 * size * args.b
+            else:
+                gx = gx_old 
+                gy = gy_old
+        # gradient tracking
+        vx = vx + gx - gx_old
+        vy = vy + gy - gy_old
+        
+        vx = fast_mix(vx, comm, rank, size, kappa, args.num_mix)
+        vy = fast_mix(vy, comm, rank, size, kappa, args.num_mix)
+            
+        gx_old = np.copy(gx)
+        gy_old = np.copy(gy)
+        # update x and y
+        x_old = np.copy(x)
+        x = x - args.lr_x * vx
+        y_old = np.copy(y)
+        y = y + args.lr_y * vy
+
+        x = fast_mix(x, comm, rank, size, kappa, args.num_mix)
+        y = fast_mix(y, comm, rank, size, kappa, args.num_mix)
+
+        y = projection(y, n)
+
+        comm_cnt = comm_cnt + 4 * args.num_mix
+    
+
+        if (epoch + 1) % args.print_freq == 0:
+            # compute x_bar on agent rank 0
+            comm.Reduce(x, x_bar, op=MPI.SUM)
+            if rank == 0:
+                x_bar = x_bar / size
+                phi,grad_phi = cal_phi(data, label, x_bar, n, lambda2, alpha)
+                with open(args.out_fname, '+a') as f:
+                    print('DREAM: {ep:d},{phi:.8f},{grad_phi:.8f}'.format(ep=epoch + 1, phi=float(phi),grad_phi=float(grad_phi)), file=f)
+                # store the minimal loss
+                if phi < min_phi:
+                    min_phi = phi
+                phi_lst.append(phi / n)
+                grad_comp_lst.append(grad_cnt)
+                comm_comp_lst.append(comm_cnt)
+        comm.barrier()
+    return grad_comp_lst, comm_comp_lst, phi_lst
